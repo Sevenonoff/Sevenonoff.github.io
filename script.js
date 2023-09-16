87a943c1ef704f5ab2041cda4b915be13092933a
Sevenonoff <cista@ya.ru>
2023-09-16 20:25:18 +0500

    v2

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 const showMoreBtn = document.getElementById('showMoreBtn');
 const additionalInfo = document.createElement('p');
-additionalInfo.textContent = 'Additional information about me.';
+additionalInfo.textContent = 'Playrix, Dublin (remote).';
 additionalInfo.style.display = 'none';
+
+const paragraph = document.getElementById('additionalInfo');
+
+function toggleVisibility() {
+  if (paragraph.style.visibility === 'hidden') {
+    paragraph.style.visibility = 'visible';
+  } else {
+    paragraph.style.visibility = 'hidden';
+  }
+}
+
+
+
 
 showMoreBtn.addEventListener('click', function() {
   if (additionalInfo.style.display === 'none') {
     additionalInfo.style.display = 'block';
-    showMoreBtn.textContent = 'Show less information';
+    showMoreBtn.textContent = 'Убрать информации, но совсем чуть-чуть';
   } else {
     additionalInfo.style.display = 'none';
-    showMoreBtn.textContent = 'Show more information';
+    showMoreBtn.textContent = 'Показать больше информации';
   }
 });
 
