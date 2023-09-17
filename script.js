1f14f13980ed14e0cd08f8334d8631c499605463
Sevenonoff <cista@ya.ru>
2023-09-17 23:30:50 +0500

    preprod

--- script.js
+++ script.js
@@ -6,10 +6,32 @@ function toggleVisibility() {
   if (paragraph.style.display === 'none') {
     showMoreBtn2.textContent = 'Убрать информации, но совсем чуть-чуть';
     paragraph.style.display = 'block';
+
+  // Get the div element
+  var div = document.querySelector('div');
+
+  // Add a class to trigger the animation
+  div.classList.add('fade-in');
+
+
   } else {
     showMoreBtn2.textContent = 'Показать больше информации';
     paragraph.style.display = 'none';
   }
+}
+
+
+function changeSlide(index) {
+  const images = document.getElementsByClassName('slider')[0].getElementsByTagName('img');
+  const dots = document.getElementsByClassName('dot');
+
+  for (let i = 0; i < images.length; i++) {
+    images[i].style.display = 'none';
+    dots[i].classList.remove('active');
+  }
+
+  images[index].style.display = 'block';
+  dots[index].classList.add('active');
 }
 
 
